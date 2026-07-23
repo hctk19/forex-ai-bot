0d25c8c43170157b36ff369ef8c0d3700dd66814
hctk19 <79217455+hctk19@users.noreply.github.com>
2026-07-23 04:51:49 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# ==========================
+# API KEYS
+# ==========================
+
+TWELVEDATA_API_KEY = "732fd8c8c17d4d7c890cc306a49cd33b"
+
+OPENAI_API_KEY = ""
+
+NEWS_API_KEY = ""
+
+TELEGRAM_TOKEN = ""
+
+TELEGRAM_CHAT_ID = ""
