c9139c1ef4f881e14a73c28572375bec44880734
hctk19 <79217455+hctk19@users.noreply.github.com>
2026-07-23 04:44:52 +0300

    market_data.py

--- market_data.py
+++ market_data.py
@@ -12,12 +12,15 @@ Kuralları:
 Sadece veri döndürür.
 """
 
+import yfinance as yf
+
 
 class MarketData:
 
-    def get_candles(self, symbol, timeframe, count):
+    def get_candles(self, symbol, timeframe="15m", count=500):
         """
         Belirtilen sembol için mum verilerini döndürür.
+        (Şimdilik daha sonra dolduracağız.)
         """
         raise NotImplementedError
 
@@ -25,16 +28,26 @@ class MarketData:
         """
         Güncel fiyatı döndürür.
         """
-        raise NotImplementedError
+
+        ticker = yf.Ticker(symbol)
+
+        data = ticker.history(period="1d", interval="1m")
+
+        if data.empty:
+            return None
+
+        return float(data["Close"].iloc[-1])
 
     def get_symbol_info(self, symbol):
         """
         Enstrüman bilgilerini döndürür.
+        (Daha sonra dolduracağız.)
         """
         raise NotImplementedError
 
     def get_volume(self, symbol):
         """
         Güncel hacim bilgisini döndürür.
+        (Daha sonra dolduracağız.)
         """
         raise NotImplementedError
